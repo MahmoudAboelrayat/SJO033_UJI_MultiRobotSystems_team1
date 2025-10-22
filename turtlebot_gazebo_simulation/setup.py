0a94fd24558c04b4a2d814e2a7a77fde47af0e8b
Mohamed Magdy <mohamedmajdi0002@gmail.com>
2025-10-23 00:47:56 +0200

    Update setup.py

--- turtlebot_gazebo_simulation/setup.py
+++ turtlebot_gazebo_simulation/setup.py
@@ -23,6 +23,7 @@ setup(
             'velocity_publisher = turtlebot_gazebo_simulation.velocity_publisher:main',
             'goal_controller = turtlebot_gazebo_simulation.goal_controller:main',
             '4_robots_cw_topology = turtlebot_gazebo_simulation.4_robots_cw_topology:main',
+            'realRobots_4_cw_topology = turtlebot_gazebo_simulation.realRobots_4_cw_topology:main',
         ],
     },
 )
